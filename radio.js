5b48babfd1f14c2e346049f10a1280c553dd4649
hamidbaki2012-collab <hamidbaki2012@gmail.com>
2026-04-12 14:00:33 +0200

    Update radio.js

--- radio.js
+++ radio.js
@@ -1,29 +1,22 @@
 const player = document.getElementById("player");
 const btn = document.getElementById("playBtn");
 
-function setPlay() {
-  btn.classList.add("not-playing");
-  btn.classList.remove("playing");
-}
-
-function setPause() {
-  btn.classList.add("playing");
-  btn.classList.remove("not-playing");
-}
-
 function togglePlay() {
   if (player.paused) {
     player.play();
-    setPause();
   } else {
     player.pause();
-    setPlay();
   }
 }
 
-// sync sécurité
-player.addEventListener("play", setPause);
-player.addEventListener("pause", setPlay);
+// quand ça joue → rouge
+player.addEventListener("play", () => {
+  btn.classList.add("active");
+  btn.textContent = "⏸ Pause";
+});
 
-// état initial
-setPlay();
+// quand ça stop → bleu
+player.addEventListener("pause", () => {
+  btn.classList.remove("active");
+  btn.textContent = "▶ Play";
+});
